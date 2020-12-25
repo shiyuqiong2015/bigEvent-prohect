75b965da26f8edf14fa07b791b1ab90283a87b04
syq <shiyuqiong2015@163.com>
2020-12-25 20:47:53 +0800

    完成发表文章、编辑文章和删除文章功能

--- 静态结构/admin/js/http.js
+++ 静态结构/admin/js/http.js
@@ -1,9 +1,13 @@
 function myAjax(type, url, data, callback) {
   let baseUrl = 'http://localhost:8080/api/v1';
+  //判断传过来的data是否为FormData的实例对象，如果是实例对象，就要设置contentType:false和processData:false,否则就默认true
+  let isDataFormData = data instanceof FormData;
   $.ajax({
     url: baseUrl + url,
     type: type,
     data: data,
+    contentType: isDataFormData ? false : 'application/x-www-form-urlencoded',
+    processData: isDataFormData ? false : true,
     headers: {
       Authorization: localStorage.getItem('bigEvent_token')
     },
@@ -11,7 +15,6 @@ function myAjax(type, url, data, callback) {
       if (resData.code == 403) {
         window.parent.location = ' ./login.html';
         return;
-
       }
       callback(resData)
     }
